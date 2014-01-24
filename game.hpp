29da3acf65891b63139d0c648c344d337011a03d
Adrian E <addis.aden@gmail.com>
2014-01-24 16:07:17 +0100

    comments for game.hpp

--- game.hpp
+++ game.hpp
@@ -8,10 +8,26 @@ namespace TicTacToe {
       int move;
 
     public:
+      // Create a new game
       Game ();
+
+      // Give a String of the field
       std::string to_string ();
+
+      // possible field = 0 - 8
+      // true = move successed
+      // false = cant move on chosen field
       bool go (int);
+
+      //  0 = Nobody wins
+      //  1 = X wins
+      //  2 = O wins
+      // -1 = Nobody can win
       int winner ();
+
+      //  0 = No move
+      //  1 = X turn
+      //  2 = O turn
       int current ();
   };
 }
